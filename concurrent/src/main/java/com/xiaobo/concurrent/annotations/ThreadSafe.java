eeca9907de8d0f99c1690530324d25dfa06f43d5
pl1111w <1362596047@qq.com>
2019-10-14 23:23:57 +0800

    second

---
+++ concurrent/src/main/java/com/xiaobo/concurrent/annotations/ThreadSafe.java
@@ -0,0 +1,15 @@
+package com.xiaobo.concurrent.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**用来标识【线程安全】的类**/
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    String value() default "";
+}
