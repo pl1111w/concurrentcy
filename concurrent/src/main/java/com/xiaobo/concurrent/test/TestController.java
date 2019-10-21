678a35c59169ed4a33064443749b9406eaac4f56
pl1111w <1362596047@qq.com>
2019-10-22 00:26:51 +0800

    the

---
+++ concurrent/src/main/java/com/xiaobo/concurrent/test/TestController.java
@@ -0,0 +1,18 @@
+package com.xiaobo.concurrent.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+   private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+   @GetMapping("/test")
+   public String printTestInfo(){
+       logger.info("test");
+       return"Test";
+   }
+}
