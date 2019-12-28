d26efbd48163f80fa77c80bae384f2e6d7d7d2fa
pl1111w <1362596047@qq.com>
2019-12-29 00:08:47 +0800

    sixteen

--- concurrent/src/main/java/com/xiaobo/concurrent/example/aqs/SemaphoreExample1.java
+++ concurrent/src/main/java/com/xiaobo/concurrent/example/aqs/SemaphoreExample1.java
@@ -2,7 +2,6 @@ package com.xiaobo.concurrent.example.aqs;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
