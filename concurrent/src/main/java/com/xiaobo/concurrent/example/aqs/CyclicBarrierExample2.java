9aae2fb6943217357555feca44d977d49ec0ddc8
pl1111w <1362596047@qq.com>
2019-12-29 00:07:10 +0800

    sixteen

---
+++ concurrent/src/main/java/com/xiaobo/concurrent/example/aqs/CyclicBarrierExample2.java
@@ -0,0 +1,44 @@
+package com.xiaobo.concurrent.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+public class CyclicBarrierExample2 {
+
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executorService.execute(() -> {
+                try {
+                    race(threadNum);
+                } catch (InterruptedException e) {
+                    log.error("InterruptedException:", e);
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+
+    private static void race(int threadNum) throws InterruptedException {
+        Thread.sleep(1000);
+        log.info(threadNum + " is ready");
+        try {
+            cyclicBarrier.await(1000, TimeUnit.MILLISECONDS);//等1秒在判断是否累计到屏障数
+            //累计到屏障数继续执行
+            log.info(threadNum + " begin race ...");
+        } catch (BrokenBarrierException | TimeoutException e) {
+            log.error("BarrierException", e);
+        }
+
+
+    }
+
+}
