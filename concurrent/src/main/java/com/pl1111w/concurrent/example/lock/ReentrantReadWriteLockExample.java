77922e45881e5839525d5c094084e513e5495ba0
Xiaobo <1362596047@qq.com>
2021-02-27 14:03:43 +0800

    ReentrantWriteReadLock

--- concurrent/src/main/java/com/pl1111w/concurrent/example/lock/ReentrantReadWriteLockExample.java
+++ concurrent/src/main/java/com/pl1111w/concurrent/example/lock/ReentrantReadWriteLockExample.java
@@ -3,14 +3,16 @@ package com.pl1111w.concurrent.example.lock;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Slf4j
 public class ReentrantReadWriteLockExample {
-    private final Map<String, Data> map = new TreeMap<>();
+    private Map<String, Integer> map = new TreeMap<>();
 
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
@@ -18,36 +20,55 @@ public class ReentrantReadWriteLockExample {
 
     private final Lock writeLock = lock.writeLock();
 
-    public Data get(String key) {
+
+    public static void main(String[] args) {
+
+        ReentrantReadWriteLockExample example = new ReentrantReadWriteLockExample();
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 1; i < 6; i++) {
+            final Integer tempInt = i;
+            executorService.execute(() -> {
+                example.put(tempInt.toString(), tempInt);
+            });
+        }
+
+
+        try {
+            TimeUnit.MICROSECONDS.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 1; i < 6; i++) {
+            final Integer tempInt = i;
+            executorService.execute(() -> {
+                example.get(tempInt.toString());
+            });
+        }
+    }
+
+
+    public int get(String key) {
         readLock.lock();
         try {
+            System.out.println(Thread.currentThread().getName() + "-开始读：" + map.get(key));
             return map.get(key);
         } finally {
             readLock.unlock();
         }
     }
 
-    public Set<String> getAllKeys() {
-        readLock.lock();
-        try {
-            return map.keySet();
-        } finally {
-            readLock.unlock();
-        }
-    }
-
-    public Data put(String key, Data value) {
+    public void put(String key, Integer value) {
         writeLock.lock();
         try {
-            return map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + "-开始写：" + key);
+            map.put(key, value);
         } finally {
-            readLock.unlock();
+            writeLock.unlock();
         }
     }
 
-    private class Data {
-
-    }
 
 }
 
