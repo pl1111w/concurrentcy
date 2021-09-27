ede908348c10695d0006a25f7e326db90229828d
xiaobo wei <1362596047@qq.com>
2021-09-27 22:37:43 +0800

    Create new thread

---
+++ concurrent/src/main/java/com/pl1111w/concurrent/example/threadTest/ThreadExample.java
@@ -0,0 +1,44 @@
+package com.pl1111w.concurrent.example.threadTest;
+
+/**
+ * @title: pl1111w
+ * @description:
+ * @author: Kris
+ * @date 2021/9/27 21:26
+ */
+public class ThreadExample {
+
+    public static void main(String[] args) {
+
+        A a = new A();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+
+        a.start();
+
+        new Thread( ){
+            @Override
+            public void run(){
+                for (int i = 0; i < 100; i++) {
+                    System.out.println(Thread.currentThread().getName() + " " + i);
+                }
+            }
+
+        }.start();
+
+    }
+
+
+}
+
+class A extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
