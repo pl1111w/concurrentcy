ff7b461940eabff9b9fc570e07f959f004ec6cf8
xiaobo wei <1362596047@qq.com>
2023-12-26 16:37:44 +0800

    thread pool

--- concurrent/src/main/java/com/pl1111w/concurrent/example/threadPool/NewCachedThreadPoolExample.java
+++ concurrent/src/main/java/com/pl1111w/concurrent/example/threadPool/NewCachedThreadPoolExample.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Slf4j
-public class CachedThreadPoolExample {
+public class NewCachedThreadPoolExample {
     /**
      * maximumPoolSize： 最大线程数为Integer.MAX_VALUE 线程创建过多导致oom
      */
