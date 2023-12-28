589ec91df90788cfcf5f6cd9f5e38ede2563af70
xiaobo wei <1362596047@qq.com>
2023-12-28 15:39:09 +0800

    ThreadLocalExample

--- concurrent/src/main/java/com/pl1111w/concurrent/ConcurrentApplication.java
+++ concurrent/src/main/java/com/pl1111w/concurrent/ConcurrentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ConcurrentApplication extends WebMvcConfigurerAdapter {
+public class ConcurrentApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConcurrentApplication.class, args);
